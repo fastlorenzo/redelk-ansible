c7ff780f66d08756450d30a8deb20fa01b04d175
fastlorenzo <git@bernardi.be>
2020-09-25 21:22:41 +0200

    Updated doc generator + playbook

--- make_doc.sh
+++ make_doc.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+roles=(redelk-server redelk-client)
 
-SAVEIFS=$IFS
-IFS=$'\n'
-variables=$(cat roles/redelk-server/defaults/main.yml | grep -v -E '(^#|---)' --color=never)
+for role in ${roles[@]}; do
+  echo -e "Processing role $role\n\n"
+  SAVEIFS=$IFS
+  IFS=$'\n'
+  variables=$(cat roles/$role/defaults/main.yml | grep -v -E '(^#|---)' --color=never)
 
-echo "| Variable | Description | Default value |"
-echo "|----------|-------------|---------------|"
-for var in $variables; do
-  key=$(echo $var | cut -d ':' -f 1)
-  val=$(echo $var | cut -d ':' -f 2- | sed -E -e 's/(^\s*)"*//g' | sed -E -e 's/"$//g')
-  descr=$(cat roles/redelk-server/defaults/main.yml | grep -E "(^#\s*${key}\s*\|)" | cut -d '|' -f 2- | sed -E -e 's/^\s*//g')
-  echo "| \`$key\` | $descr | \`$val\` |"
+  echo "| Variable | Description | Default value |"
+  echo "|----------|-------------|---------------|"
+  for var in $variables; do
+    key=$(echo $var | cut -d ':' -f 1)
+    val=$(echo $var | cut -d ':' -f 2- | sed -E -e 's/(^\s*)"*//g' | sed -E -e 's/"$//g')
+    descr=$(cat roles/$role/defaults/main.yml | grep -E "(^#\s*${key}\s*\|)" | cut -d '|' -f 2- | sed -E -e 's/^\s*//g')
+    echo "| \`$key\` | $descr | \`$val\` |"
+  done
+  echo -e "\n\n"
+  IFS=$SAVEIFS
+
 done
-
-IFS=$SAVEIFS
