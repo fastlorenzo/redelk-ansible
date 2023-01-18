332a3ad5c770f079df0c85a0327e7775e2de9aec
fastlorenzo <git@bernardi.be>
2023-01-18 09:51:15 +0100

    Fixed linting
    Signed-off-by: fastlorenzo <git@bernardi.be>

--- README.md
+++ README.md
@@ -5,12 +5,13 @@ This repository contains multiple Ansible playbook allowing to deploy [RedELK](h
 All credits goes to the authors and contributors of RedELK.
 
 The roles can be found on the following repositories:
+
 - [redelk-server](https://github.com/fastlorenzo/redelk-server)
 - [redelk-client](https://github.com/fastlorenzo/redelk-client)
 
 ## Install requirements
 
-```
+```bash
 ansible-galaxy install -r requirements.yml
 ansible-galaxy collection install community.crypto
 ansible-galaxy collection install community.general
@@ -18,13 +19,13 @@ ansible-galaxy collection install community.general
 
 ## Deploy RedELK
 
-```
+```bash
 ansible-playbook -i inventory install-redelk.yml -K
 ```
 
 ## Remove systemd-resolved stub listener (to free port 53 for DNS redirectors)
 
-```
+```bash
 sed -i 's/#DNSStubListener=yes/DNSStubListener=no/g /etc/systemd/resolved.conf'
 sudo rm /etc/resolv.conf
 sudo ln -s /run/systemd/resolve/resolv.conf /etc/
--- utils/check_changes.py
+++ utils/check_changes.py
@@ -8,77 +8,100 @@ from pprint import pprint
 
 try:
     from colorama import Fore, Back, Style, init
+
     init()
 except ImportError:  # fallback so that the imported classes always exist
-    class ColorFallback():
-        __getattr__ = lambda self, name: ''
+
+    class ColorFallback:
+        def __getattr__(self, name):
+            return ""
+
     Fore = Back = Style = ColorFallback()
+
 
 def color_diff(diff):
     for line in diff:
-        if line.startswith('+'):
+        if line.startswith("+"):
             yield Fore.GREEN + line + Fore.RESET
-        elif line.startswith('-'):
+        elif line.startswith("-"):
             yield Fore.RED + line + Fore.RESET
-        elif line.startswith('^'):
+        elif line.startswith("^"):
             yield Fore.BLUE + line + Fore.RESET
         else:
             yield line
 
-def check_redelk_scripts(redelkpath, rolespath, redelkdir, ansiblepath):
-    print('Checking RedELK scripts (%s)' % ansiblepath)
-    redelk_files = [f for f in os.listdir('%s/%s' % (redelkpath, redelkdir)) if isfile(join('%s/%s' % (redelkpath, redelkdir), f))]
+
+def check_redelk_scripts(redelk_path, roles_path, redelk_dir, ansible_path):
+    print(f"Checking RedELK scripts ({ansible_path})")
+    redelk_files = [
+        f
+        for f in os.listdir(f"{redelk_path}/{redelk_dir}")
+        if isfile(join(f"{redelk_path}/{redelk_dir}", f))
+    ]
 
     files = {}
-    for rf in redelk_files:
-        to_check = '%sredelk-server/files/%s%s' % (rolespath, ansiblepath, rf)
+    for redelk_file in redelk_files:
+        to_check = f"{roles_path}redelk-server/files/{ansible_path}{redelk_file}"
         ftype = None
-        #print('Checking if file %s exists' % to_check)
+        # print('Checking if file %s exists' % to_check)
         if os.path.exists(to_check):
             af = to_check
-            ftype = 'file'
+            ftype = "file"
         else:
-            to_check = '%sredelk-server/templates/%s%s.j2' % (rolespath, ansiblepath, rf)
-            #print('Checking if file %s exists' % to_check)
+            to_check = (
+                f"{roles_path}redelk-server/templates/{ansible_path}{redelk_file}.j2"
+            )
+            # print('Checking if file %s exists' % to_check)
             if os.path.exists(to_check):
                 af = to_check
-                ftype = 'template'
+                ftype = "template"
 
         if ftype is None:
-            print('New file not in Ansible redelk-server: %s' % rf)
-            files[rf] = {
-                'new': True,
-                'diff': ''
-            }
+            print(f"New file not in Ansible redelk-server: {redelk_file}")
+            files[redelk_file] = {"new": True, "diff": ""}
         else:
-            src_file = '%s/%s/%s' % (redelkpath, redelkdir, rf)
-            dst_file = '%sredelk-server/%ss/%s%s%s' % (rolespath, ftype, ansiblepath, rf, '.j2' if ftype == 'template' else '')
-            print('%s########### [%s] -> [%s]%s' % (Fore.CYAN, src_file, dst_file, Fore.RESET))
+            src_file = f"{redelk_path}/{redelk_dir}/{redelk_file}"
+            dst_file = "{rolespath}redelk-server/{ftype}s/{ansiblepath}{redelk_file}{'.j2' if ftype == 'template' else ''}"
+            print(f"{Fore.CYAN}########### [{src_file}] -> [{dst_file}]{Fore.RESET}")
 
-            src_data = open(src_file).readlines()
-            dst_data = open(dst_file).readlines()
+            src_data = open(src_file, encoding="utf-8").readlines()
+            dst_data = open(dst_file, encoding="utf-8").readlines()
 
             diff = difflib.unified_diff(dst_data, src_data)
             diff = color_diff(diff)
-            diff = ''.join(diff)
-            files[rf] = {
-                'new': False,
-                'diff': diff
-            }
+            diff = "".join(diff)
+            files[redelk_file] = {"new": False, "diff": diff}
             print(diff)
 
-    print('******** Summary for %s ********' % ansiblepath)
+    print(f"******** Summary for {ansible_path} ********")
     for f in files:
-        print('%s%s: %d' % ('+ ' if files[f]['new'] else '', f, len(files[f]['diff'])))
-    print('***************************************************\n')
+        print(f'{"+ " if files[f]["new"] else ""}{f}: {len(files[f]["diff"])}')
+    print("***************************************************\n")
+
 
 def check_args():
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--redelkpath", metavar="<redelkpath>", dest="redelkpath", default='../../RedELK/', help="Path to RedELK directory (default ../../RedELK/)")
-    parser.add_argument("--rolespath", metavar="<rolespath>", dest="rolespath", default='../roles/', help="Path to RedELK roles directory (default ../roles/)")
-    parser.add_argument("--client", action='store_true', help="Check client components")
-    parser.add_argument("--server", action='store_true', help="Check server components")
-    parser.add_argument("--all", action='store_true', help="Check all components (similar to --client --server)")
+    parser.add_argument(
+        "--redelkpath",
+        metavar="<redelkpath>",
+        dest="redelkpath",
+        default="../../RedELK/",
+        help="Path to RedELK directory (default ../../RedELK/)",
+    )
+    parser.add_argument(
+        "--rolespath",
+        metavar="<rolespath>",
+        dest="rolespath",
+        default="../roles/",
+        help="Path to RedELK roles directory (default ../roles/)",
+    )
+    parser.add_argument("--client", action="store_true", help="Check client components")
+    parser.add_argument("--server", action="store_true", help="Check server components")
+    parser.add_argument(
+        "--all",
+        action="store_true",
+        help="Check all components (similar to --client --server)",
+    )
     args = parser.parse_args()
 
     if not args.client and not args.server and not args.all:
@@ -87,12 +110,20 @@ def check_args():
 
     return args
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     args = check_args()
 
-    redelkpath = args.redelkpath if args.redelkpath else '../../RedELK/'
-    rolespath = args.rolespath if args.rolespath else '../roles/'
+    redelkpath = args.redelkpath if args.redelkpath else "../../RedELK/"
+    rolespath = args.rolespath if args.rolespath else "../roles/"
 
-    check_redelk_scripts(redelkpath, rolespath, 'elkserver/scripts/','usr/share/redelk/bin/')
-    check_redelk_scripts(redelkpath, rolespath, 'elkserver/logstash/ruby-scripts/','etc/logstash/ruby-scripts/')
+    check_redelk_scripts(
+        redelkpath, rolespath, "elkserver/scripts/", "usr/share/redelk/bin/"
+    )
+    check_redelk_scripts(
+        redelkpath,
+        rolespath,
+        "elkserver/logstash/ruby-scripts/",
+        "etc/logstash/ruby-scripts/",
+    )
